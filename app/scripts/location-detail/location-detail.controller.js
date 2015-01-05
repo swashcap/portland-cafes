06c334f5c2fe31205e48392ba6b3655fe55ec54d
Cory Reed <swashcap@gmail.com>
2015-01-04 21:17:41 -0800

    Add time awareness to location detail view.

--- app/scripts/location-detail/location-detail.controller.js
+++ app/scripts/location-detail/location-detail.controller.js
@@ -4,5 +4,17 @@
   angular.module('portlandcafes')
     .controller('LocationDetailCtrl', ['$scope', '$routeParams', 'Locations', function ($scope, $routeParams, Locations) {
       $scope.location = Locations.get($routeParams.locationId);
+      $scope.isClosed = false;
+      $scope.isClosingSoon = false;
+
+      // Set up time-related `$scope` properties
+      var now = new Date();
+      now = now.getHours() + now.getMinutes() / 60;
+
+      if ($scope.location.hours.open > now || now > $scope.location.hours.close) {
+        $scope.isClosed = true;
+      } else if (now + .5 >= location.hours.close) {
+        $scope.isClosingSoon = true;
+      }
     }]);
 })(window.angular);
