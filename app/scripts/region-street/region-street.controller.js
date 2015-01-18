1884ac297cb60acfc1361af8f8e9dc7d495fedd5
Cory Reed <swashcap@gmail.com>
2015-01-18 13:44:13 -0800

    Center Street map on its locations.

--- app/scripts/region-street/region-street.controller.js
+++ app/scripts/region-street/region-street.controller.js
@@ -2,7 +2,21 @@
   'use strict';
 
   angular.module('portlandcafes')
-    .controller('RegionStreetCtrl', ['$scope', '$routeParams', 'Streets', 'Locations', function ($scope, $routeParams, Streets, Locations) {
+    .controller('RegionStreetCtrl', ['$scope', '$routeParams', 'Streets', 'Locations', 'Geolib', function ($scope, $routeParams, Streets, Locations, Geolib) {
+      /**
+       * @todo This controller shares much with the 'RegionSingleCtrl'. Look
+       *       into a manner of sharing functions, initialization, etc.
+       */
+
+      var getCenterObject = function (center) {
+        return {
+          coords: {
+            latitude: center.latitude,
+            longitude: center.longitude
+          }
+        };
+      };
+
       $scope.street = Streets.filter(function (street) {
         return street.slug === $routeParams.streetName;
       }).shift();
@@ -18,6 +32,13 @@
             }
           }
         });
+
+        $scope.center = getCenterObject(Geolib.getCenter($scope.locations.map(function (location) {
+          return {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+          };
+        })));
       });
     }]);
 })(window.angular);
