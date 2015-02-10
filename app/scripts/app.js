b417a9a308b735fa8b85b232a408872b7d0bab38
Cory Reed <swashcap@gmail.com>
2015-02-09 19:24:00 -0800

    Move location loading and instantiation out of `Locations`.
    This moves loading and parsing of the location JSON from the
    `Location` component to an Angular `run` block. This
    makes the `Location` component more focused and promotes
    a better app lifecycle and organization.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,56 @@
 (function (angular) {
   'use strict';
 
-  angular.module('portlandcafes', ['ngRoute', 'ngSanitize']);
+  angular.module('portlandcafes', ['ngRoute', 'ngSanitize'])
+    .run(['$http', 'Hours', 'Locations', function ($http, Hours, Locations) {
+      $http.get('results.json').then(function (data) {
+        var results = [];
+
+        if ('data' in data && Array.isArray(data.data)) {
+          /*jshint camelcase: false */
+          results = data.data.map(function (result) {
+            var output = {
+              address: result.adr_address,
+              phoneNumber: result.formatted_phone_number,
+              coords: {
+                latitude: result.geometry.location.lat,
+                longitude: result.geometry.location.lng
+              },
+              id: result.id,
+              name: result.name,
+              placeId: result.place_id,
+              rating: result.rating,
+              reviews: result.reviews,
+              types: result.types,
+              url: result.url,
+              vicinity: result.vicinity,
+              website: result.website
+            };
+
+            var periods = (((result).opening_hours || {}).periods || []);
+
+            if (periods.length) {
+              output.isOpen = Hours.isOpen(periods);
+              output.isClosingSoon = Hours.isClosingSoon(periods);
+              output.todayHours = {
+                open: Hours.getOpenTime(periods),
+                close: Hours.getCloseTime(periods)
+              };
+              output.hours = [];
+
+              [0, 1, 2, 3, 4, 5, 6].forEach(function (number) {
+                output.hours.push(Hours.getHoursByDay(periods, number));
+              });
+            }
+            /*jshint camelcase: true */
+
+            return output;
+          });
+        }
+
+        Locations._setLocations(results);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    }]);
 })(window.angular);
