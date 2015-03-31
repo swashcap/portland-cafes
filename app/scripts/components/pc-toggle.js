e9229b678b965e93f723d1ef02c0a2b31e0ef905
Cory Reed <swashcap@gmail.com>
2015-03-30 20:19:26 -0700

    Add toggler directives.

--- app/scripts/components/pc-toggle.js
+++ app/scripts/components/pc-toggle.js
@@ -8,20 +8,24 @@
       var SEARCH_CLASS = 'is-search';
       var LOCATION_CLASS = 'is-location';
       var $site = $document.find('.site');
+      var $searchField = $('.search-form input');
 
       return {
         link: function (scope, element, attrs) {
           var toggle = attrs.pcToggle;
 
           element.on('click', function (e) {
-            e.preventDefault();
-
             switch (toggle) {
               case 'menu':
                 $site.toggleClass(MENU_CLASS);
                 break;
               case 'search':
-                $site.toggleClass(SEARCH_CLASS);
+                if ($site.hasClass(SEARCH_CLASS)) {
+                  $site.removeClass(SEARCH_CLASS);
+                } else {
+                  $site.addClass(SEARCH_CLASS);
+                  $searchField.focus();
+                }
                 break;
               case 'location':
                 $site.toggleClass(LOCATION_CLASS);
