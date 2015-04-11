9ceec0e572c82091d9200672f09c230e1f460a4a
Cory Reed <swashcap@gmail.com>
2015-04-11 11:07:38 -0700

    Add test file for IndexedDB component.
    This component is hard to test because it relies on the IDBWrapper 3rd party
    library, which doesn't make itself easily testable. More research is needed.

---
+++ app/scripts/components/storage/indexed-db.spec.js
@@ -0,0 +1,46 @@
+describe('IndexedDB Component', function () {
+  'use strict';
+
+  /**
+   * Use `mock-indexeddb` for testing the IndexedDB implementation.
+   * @{@link  https://github.com/szimmers/mock-indexeddb}
+   */
+  var location1 = {
+    id: 1,
+    name: 'Test Location 1',
+    region: 'north',
+    isOpen: false
+  };
+  var location2 = {
+    id: 2,
+    name: 'Test Location 2',
+    region: 'north',
+    isOpen: true
+  };
+  var location3 = {
+    id: 3,
+    name: 'Test Location 3',
+    region: 'downtown',
+    isOpen: true
+  };
+
+  beforeEach(function () {
+    module('pcStorage');
+
+    resetIndexedDBMock();
+    commitIndexedDBMockData(location1.id, location1);
+    commitIndexedDBMockData(location2.id, location2);
+    commitIndexedDBMockData(location3.id, location3);
+  });
+
+  xit('should store locations', function (done) {
+    inject(function (IndexedDB) {
+      IndexedDB.put([location1, location2, location3]).then(function (transaction) {
+        console.log('Complete!', transaction);
+        done();
+      }).catch(function (error) {
+        console.log('Error!', error);
+      });
+    });
+  });
+});
